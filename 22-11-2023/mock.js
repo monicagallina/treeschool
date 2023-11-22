300c72e5b93447e72c4823c9ceec4be2975baf66
monicagallina <monica.gallina7@gmail.com>
2023-11-22 11:58:36 +0100

    22-11-2023-2

--- 22-11-2023/mock.js
+++ 22-11-2023/mock.js
@@ -5,16 +5,20 @@
 //Esercizio 5 dato il mock di partenza, stampa in console solo i nomi dei prodotti presenti nel mock
 
 
-// es 1
 
 import products from "./mock.json" assert {type: "json"};
 
+// Esercizio 1 dato il mock di partenza, stampa in console la lista dei prodotti;
 console.log(products.productsList);
 
-// 2
+// Esercizio 2 dato il mock di partenza, stampa in console il terzo oggetto della lista dei prodotti, cioé le cuffie...
 console.log(products.productsList[2])
 
-//3
+///Esercizio 3 dato il mock di partenza, stampa in consolela lista dei prodotti che costano meno di 200 euro
+
+console.log(products.productsList.filter((products) => products.price<200))
+
+// 3 con ciclo
 function costsLess(element){
     for (let i=0; i<products.productsList.length; i++){
     if (productsList[i].price < 200){
@@ -24,8 +28,15 @@ function costsLess(element){
 }
 }
 
+// Esercizio 4 dato il mock di partenza, stampa in console il prodotto con id 8, (ATTENZIONE:come oggetto non come lista)
+console.log(products.productsList.filter((el) => el.id === 8));
 
-//4 
+
+//4 correzione
+console.log(products.productsList.filter((el) => el.id === 8)[i]);
+
+
+//4 con ciclo
 function findId(){
     for (let j=0; j<products.productsList.length; j++){
         if (products.productsList[j].id === 8){
@@ -36,7 +47,13 @@ function findId(){
 
 findId()
 
-//5 
+
+//Esercizio 5 dato il mock di partenza, stampa in console solo i nomi dei prodotti presenti nel mock
+console.log(products.productsList.forEach((el)=> el.name));
+
+
+
+//5 con ciclo
 
 function printNames(){
     for (let d=0; d<products.productsList.length; d++){
@@ -45,3 +62,6 @@ function printNames(){
 }
 
 printNames()
+
+//5 correzione 
+products.productsList.map((prod)=> console.log(prod.name))
